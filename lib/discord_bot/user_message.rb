edf0830c4e4f68633ee94f72f4208b849438ad3d
Josh Buker <crypto@joshbuker.com>
2024-05-19 18:06:13 -0700

    Fix responses breaking if above discord limit of 2000

--- lib/discord_bot/user_message.rb
+++ lib/discord_bot/user_message.rb
@@ -12,12 +12,36 @@ module DiscordBot
       @event.server
     end
 
+    def user
+      @event.user
+    end
+
     def message
       @event.message
     end
 
     def reply_with(response)
-      @event.message.reply!(response)
+      return if response.size.zero?
+
+      if response.size <= 2000
+        @event.message.reply!(response)
+      else
+        responses = chop(response, 2000)
+        respond_to = @event.message
+        responses.each do |chopped_response|
+          next if chopped_response.nil?
+          respond_to = respond_to.reply!(chopped_response)
+        end
+      end
+    end
+
+    private
+
+    def chop(string, size)
+      count = (string.size-1 / size)
+      (0..count).map do |i|
+        string[i*size, size]
+      end
     end
   end
 end
